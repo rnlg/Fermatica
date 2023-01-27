4cf1d96e85b4134ed1b0bd6aec5d4dd9efc2a9fd
Roman N. Lee <roman.n.lee@gmail.com>
2023-01-27 10:48:14 +0700

    1. Improved makeShortcut.m 2. Switched from .m to .wl extension.

--- source/makeShortcut.m
+++ source/makeShortcut.m
@@ -9,6 +9,8 @@ Modification of PACKAGE below should be sufficient when adjusting script to new 
 PACKAGE="Fermatica";
 
 SetDirectory[DirectoryName[$InputFileName]]
+
+
 appdir=$UserBaseDirectory <> "/Applications/"<>PACKAGE<>"/";
 file = Join[FileNames[PACKAGE<>"*.wl"],FileNames[PACKAGE<>"*.m"]];
 If[file==={},Print["No "<>PACKAGE<>"*.m or "<>PACKAGE<>"*.wl in "<>Directory[]<>". Changed nothing, quitting..."];Quit[]];
@@ -25,5 +27,10 @@ WriteString[init,"Begin[\""<>PACKAGE<>"`Private`\"]\n\
 End[];"
 ]
 Close[init]
-Print["Installed shortcut for "<>file<>".\nTo load the package, use\n  <<"<>PACKAGE<>"`"]
+Print["Installed shortcut for "<>file<>".\nTo load the package, use\n  \033[1;34m<<"<>PACKAGE<>"`\033[0m"]
+fermatpath=Environment["FERMATPATH"];
+If[fermatpath===$Failed,
+Print["\033[1;35m","Warning: FERMATPATH environment variable is not set. Set FERMATPATH=/path/to/fer64 or use $FermatCMD=\"/path/to/fer64\" after <<"<>PACKAGE<>"`","\033[0m"],
+Print["\033[34m","FERMATPATH environment variable is set to \""<>fermatpath,"\"\033[0m"]
+];
 Quit[]
